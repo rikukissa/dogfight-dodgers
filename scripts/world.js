26422ae4fc41ca01a601d0de5d75f053afaa458d
Riku Rouvila <riku.rouvila@gmail.com>
2015-07-22 18:29:59 +0300

    add mock crates

--- scripts/world.js
+++ scripts/world.js
@@ -1,17 +1,18 @@
 export const WIDTH = 1000;
 export const HEIGHT = 100;
-export const GROUND_LEVEL = 1;
+export const GROUND_LEVEL = 0;
 
 export const CRATE_WIDTH = 1;
 
 export const initial = {
   crates: [
-    {
-      position: {
-        x: 10,
-        y: GROUND_LEVEL
-      }
-    }
+    {position: {x: 10, y: GROUND_LEVEL}},
+    {position: {x: 11, y: GROUND_LEVEL}},
+    {position: {x: 12, y: GROUND_LEVEL}},
+    {position: {x: 11, y: GROUND_LEVEL + 1}},
+    {position: {x: 25, y: GROUND_LEVEL}},
+    {position: {x: 26, y: GROUND_LEVEL}},
+    {position: {x: 45, y: GROUND_LEVEL}}
   ],
   sounds: {}
 };
