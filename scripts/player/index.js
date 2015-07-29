3c8e62f65a3976faf6820fce1de7b38f83f3e711
Riku Rouvila <riku.rouvila@gmail.com>
2015-07-29 21:31:57 +0300

    better player controls

--- scripts/player/index.js
+++ scripts/player/index.js
@@ -1,50 +1,47 @@
 import {GROUND_LEVEL} from 'world';
 import {RADIAN} from 'constants';
-import {create, HEIGHT} from 'plane';
+import {create} from 'plane';
 
-const MAX_SPEED = 0.5;
-const KEY_ROTATION = 0.0698;
-const STALL_ROTATION = 0.00698;
-
-
-function mod(num, from) {
-  return (num % from + from) % from;
-}
-
-function stallPlane(player, delta) {
-  const normalized = mod(player.body.angle + RADIAN * 0.25, RADIAN);
-  const distance = 180 - normalized;
-  return player.body.angle + distance * (STALL_ROTATION - STALL_ROTATION * player.healt) * delta;
-}
+const MAX_SPEED = 0.7;
+const KEY_ROTATION = 0.05;
 
 export function initial(world) {
   return create(null, world);
 }
 
+function rotatePlane(player, degrees) {
+  const y = player.body.position[1];
+
+  if(y < 2) {
+    const deltaSpeed = player.thrust / MAX_SPEED;
+    return ((degrees / 10) * deltaSpeed);
+  }
+
+  if(y < 4) {
+    return degrees * y / 4 * 0.5;
+  }
+
+  return degrees;
+}
+
 export function update(player, input) {
 
-  if(input.keys.up) {
-    player.throttle += 0.001 * input.delta;
-  } else {
-    player.throttle = 0;
-  }
+  if(input.keys.down) {
+    player.body.angle += rotatePlane(player, -KEY_ROTATION) * input.delta;
 
-  if(input.keys.left) {
-
-    const tailY = player.body.position[1] + 0.15 - (HEIGHT / 2) *
-      Math.sin(player.body.angle - RADIAN / 2);
-
-    if(tailY > 0) {
-      player.body.angle -= KEY_ROTATION * input.delta;
+    if(player.thrust < MAX_SPEED) {
+      player.thrust += 0.005 * input.delta;
     }
-  } else if(input.keys.right) {
-    player.body.angle += KEY_ROTATION * input.delta;
-  } else if(player.body.position[1] > GROUND_LEVEL) {
-    player.body.angle = stallPlane(player, input.delta);
+
+  } else if(input.keys.up) {
+    player.body.angle += rotatePlane(player, KEY_ROTATION) * input.delta;
   }
 
-  player.thrust = Math.min(MAX_SPEED, player.thrust + player.throttle);
-  player.thrust = Math.max(0, player.thrust + 0.01 * Math.max(0, Math.sin(player.body.angle)));
+
+  player.thrust = Math.min(MAX_SPEED, player.thrust);
+
+  const deltaAngle = Math.sin(player.body.angle - RADIAN / 2);
+  player.thrust -= 0.005 * deltaAngle;
 
 
   if(player.body.position[1] <= GROUND_LEVEL) {
